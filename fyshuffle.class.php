a0b001a019b0cc979d6a4a9635842e4403348793
Rob van Bentem <robvanbentem@gmail.com>
2011-11-01 13:29:25 -0700

    Added shuffle methods

--- fyshuffle.class.php
+++ fyshuffle.class.php
@@ -3,7 +3,7 @@
  *
  * FYShuffle - A Fisher-Yates PHP implementation.
  * 
- * Copyright (C) 2011  Rob van Bentem (http://github.com/robvanbentem/FYShuffle)
+ * Copyright (C) 2011	Rob van Bentem (http://github.com/robvanbentem/FYShuffle)
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,89 +12,150 @@
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.	If not, see <http://www.gnu.org/licenses/>.
  *
- */
-
-/**
- * Fisher-Yates shuffle implementation
+ *
+ *
+ * @date Tuesday, November 01 2011
+ * @version 1.0
  **/
 class FYShuffle
 {
-  // Used to store minimum and maximum range.
-  private $min, $max; 
+	// Store minimum and maximum range.
+	private $min, $max; 
 
-  // Total size ($max-$min) of range.
-  private $size;
+	// Total size ($max-$min) of range.
+	private $size;
 
-  // Cursor is the ceiling of the 'left-over' values.
-  private $ceiling;
+	// Keep track of the used/unused index posituib
+	private $ceiling;
 
-  // The values of our min-max range
-  private $values;
+	// The values of our min-max range
+	private $values;
 
-  // The user-defined array
-  private $user_array = null;
+	// The user-defined array
+	private $user_array = null;
 
-  /**
-   * Ctor determines the 'mode' of the shuffle.
-   *
-   * 1. Pass min/max integers to create a range of numbers to pick from.
-   * 2. Pass an array to use your own array to pick from
-   *
-   * @return void
-   * @author Rob van Bentem <robvanbentem@gmail.com>
-   **/
-  function __construct($min = null, $max = null){
-    if(is_numeric($min) && is_numeric($max)){
-      $this->_init_numeric($min, $max);
-    } elseif(is_array($min) && $max === null){
-      $this->_init_array($min);
-    }
+	/**
+	 * ctor determines the 'mode' of the shuffle.
+	 *
+	 * 1. Pass min/max integers to create a range of numbers to pick from.
+	 * 2. Pass an array to use your own array to pick from
+	 *
+	 * @return void
+	 **/
+	function __construct($min = null, $max = null){
+		if(is_numeric($min) && is_numeric($max)){
+      if($min >= $max) {
+        throw new exception('Max must be greater then min.');
+      }
+			$this->_init_numeric($min, $max);
+		} elseif(is_array($min) && $max === null){
+			$this->_init_array($min);
+		} else {
+			throw new exception('Please provide min/max integers or an array.');
+		}
 
-    $this->ceiling = $this->size - 1;
-  }
+		$this->ceiling = $this->size - 1;
+	}
 
-  /**
-   * Initializes the class in 'range' mode.
-   **/
-  private function _init_numeric($min, $max)
-  {
-    $this->mode = 'range';
+	/**
+	 * Initializes the class in 'range' mode.
+	 *
+	 * @return void
+	 **/
+	private function _init_numeric($min, $max)
+	{
+		$this->mode = 'range';
 
-    $this->min = $min;
-    $this->max = $max;
+		$this->min = $min;
+		$this->max = $max;
 
-    $this->size = $this->max - $this->min + 1;
+		$this->size = $this->max - $this->min + 1;
 
-    // Fill our values array with numbers between min-max.
-    for($i = 0 ; $i < $this->size; $i++) {
-      $this->values[$i] = $this->min+$i;
-    }
-  }
+		// Fill our values array with numbers between min-max.
+		for($i = 0 ; $i < $this->size; $i++) {
+			$this->values[$i] = $this->min+$i;
+		}
+	}
 
-  /**
-   * Initializes the class for user-defined array mode.
-   **/
-  private function _init_array($array)
-  {
-    $this->mode = 'array';
+	/**
+	 * Initializes the class for user-defined array mode.
+	 *
+	 * @return void
+	 **/
+	private function _init_array($array)
+	{
+		$this->mode = 'array';
 
-    $this->user_array = $array;
-    $this->size = sizeof($array);
+		$this->user_array = $array;
+		$this->size = sizeof($array);
 
-    $this->values = array();
-    
-    // Here we will create a lookup array.  
-    foreach ($array as $n => $value) {
-      $this->values[] = $n;
-    }
-  }
+		$this->values = array();
 
+		$this->min = 0;
+		$this->max = $this->size-1;
+
+		// Here we will create a lookup array.	
+		foreach ($array as $n => $value) {
+			$this->values[] = $n;
+		}
+	}
+
+
+	/**
+	 * Returns a random element from the bag
+	 *
+	 * @return void
+	 **/
+	public function fetch()
+	{
+		if($this->mode === 'range'){
+			return $this->_fetch_range();
+		} elseif($this->mode === 'array'){
+			return $this->_fetch_array();
+		}
+	}
+
+	/**
+	 * Returns a number from the range array
+	 *
+	 * @return int
+	 **/
+	private function _fetch_range()
+	{
+		// Only one element left, return it and reset the bag
+		if($this->ceiling === 0){
+			$this->ceiling = $this->size-1;
+			return reset($this->values);
+		}
+
+		// Pick a random index from range [0, $ceiling].
+		$index = mt_rand() % $this->ceiling;
+		$chosen = $this->values[$index];
+
+		// Swap chosen with the ceiling element
+		$this->values[$index] = $this->values[$this->ceiling];
+		$this->values[$this->ceiling] = $chosen;
+
+		$this->ceiling--;
+
+		return $chosen;
+	}
+
+	/**
+	 * Returns a random element from the users provided array
+	 *
+	 * @return unknown
+	 **/
+	private function _fetch_array()
+	{
+		return $this->user_array[$this->_fetch_range()];
+	}
 } // END class FYShuffle
 
 ?>
---
+++ test/test.php
@@ -0,0 +1,58 @@
+<?php
+
+require '../fyshuffle.class.php';
+
+
+// Check the difference between max and min, this should me no more then 1 at any time.
+function test($test){
+	if(abs(min($test) - max($test)) > 1) return false;
+	return true;
+}
+
+
+
+
+
+// Check range mode
+$test = array();
+for ($i = 1; $i < 11; $i++) {
+	$test[$i] = 0;
+}
+
+$shuffle = new FYShuffle(1, 10);
+for ($i = 0; $i < 100000; $i++) {
+	$result = $shuffle->fetch();
+	$test[$result]++;
+
+	if(!test($test)) {
+		die('test failed');		
+	}
+}
+
+echo 'range ok'.PHP_EOL;
+
+
+
+
+
+// Check array mode
+$array = array('one' => 'blue', 'two' => 'yellow', 'three' => 'red', 'four' => 'green', 'five' => 'orange');
+
+$test = array();
+foreach ($array as $x => $y) {
+	$test[$y] = 0;
+}
+
+$shuffle = new FYShuffle($array);
+for ($i = 0; $i < 100000; $i++) {
+	$result = $shuffle->fetch();
+	$test[$result]++;
+
+	if(!test($test)) {
+		print_r($test);
+		die('test failed');		
+	}
+}
+
+echo 'array ok'.PHP_EOL;
+?>
